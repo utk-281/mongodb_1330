f7ee08f5c29b85eaaae21e247385630abf680aee
utkarsh <utkarshgupta.281@gmail.com>
2025-03-26 09:02:50 +0530

    add intro to mongo

---
+++ arrays.js
@@ -0,0 +1,34 @@
+// let, var, const
+
+let arrNames = ["john", "doe", "tom", "jane"];
+// console.log(arrNames[5]);
+
+// length of arr => total number of elements
+//! last index ==> length -1
+
+let arrNum = [1, 2, 3, 4, 4.5, 5, 6];
+
+let obj = {
+  key1: "value1",
+  key2: 123,
+  key3: true,
+  key4: undefined,
+  key5: null,
+  key6: function () {}, // method
+  key7: {},
+  key8: [],
+};
+
+// let emp = {
+//   name: "john",
+//   id: 34,
+//   1: "value1",
+//   2: "value2",
+// };
+
+// emp.name = "new value";
+// emp.id = 122;
+// console.log(emp);
+// console.log(emp.1);
+// console.log(emp[1]);
+// console.log(emp["name"]);
